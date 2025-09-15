6ca9c1bc0782920247160344636492a723ad74db
Rampratap Palanisamy <23ita44@karpagamtech.ac.in>
2025-09-15 10:37:02 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import Conditional from "./component/Conditional";
+import Stcomp from "./component/Stcomp";
+import Home from "./component/Home";
+import Procomp from "./component/Procomp";
+import LocalStorageDemo from "./LocalStorageDemo";
+import Twowaybind from "./component/Twowaybind";
+import Apiinteg from "./component/Apiinteg";
+import Idcard from "./component/Idcard"; 
+
 function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/Home">Home || </Link>
+        <Link to="/conditional">Conditional || </Link>
+        <Link to="/stcomp">Stcomp || </Link>
+        <Link to="/props">Props || </Link>
+        <Link to="/local">Local || </Link>
+        <Link to="/twoway">Twoway || </Link>
+        <Link to="/idcard">ID Card || </Link> 
+        <Link to="/api">API || </Link>
+      </nav>
 
-  return(
-    <div>
-      <BrowserRouter>
-        <nav>
-          <link to={"/home"}>Home || </link>
-          <link to={"/conditional"}>Conditional || </link>
-          <link to={"/state"}>UseState || </link>
-          <link to={"/props"}>Props || </link>
-          <link to={"/local"}>local || </link>
-        </nav>
-
-        <Routes>
-          <Route path='/Home' element={<Home/>}/>
-          <Route path='/Conditional' element={<Conditional isLoggedin={true}/>}/>
-          <Route path='/state' element={<Stcomp/>}/>
-          <Route path='/props' element={<Stcomp name={"value1"} messa={"value2"}/>}/>
-          <Route path='/local' element={<LocalStorageDemo/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/conditional" element={<Conditional isloggedin={true} />} />
+        <Route path="/stcomp" element={<Stcomp />} />
+        <Route path="/props" element={<Procomp name="welcome" messa="login" />} />
+        <Route path="/local" element={<LocalStorageDemo />} />
+        <Route path="/twoway" element={<Twowaybind />} />
+        <Route path="/idcard" element={<Idcard />} />  
+        <Route path="/api" element={<Apiinteg />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
